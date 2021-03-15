6140951559eb1da786e66e2c3df3007d7b8fe699
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-03-15 11:08:28 -0400

    Add aria

--- application/static/js/login.js
+++ application/static/js/login.js
@@ -1,22 +1,21 @@
 // Toggle password hidden or visible.
 
 
-const passwordText = document.querySelector('.password__field');
+const passwordField = document.querySelector('.password__field');
 const eyeButton = document.querySelector('.eye__button');
-let passwordVisible = false;
 
 const togglePasswordVisible = () => {
-    if (passwordVisible === false) {
+    if (passwordField.type === 'password') {
         document.getElementById('eye__closed_icon').style.display = 'none';
         document.getElementById('eye__opened_icon').style.display = 'block';
         document.querySelector('.password__field').type = 'text';
-        passwordVisible = true;
+        eyeButton.setAttribute('aria-label', 'Hide password');
         return;
     } else {
         document.getElementById('eye__closed_icon').style.display = 'block';
         document.getElementById('eye__opened_icon').style.display = 'none';
         document.querySelector('.password__field').type = 'password';
-        passwordVisible = false;
+        eyeButton.setAttribute('aria-label', 'Make password visible');
         return;
     }
 } 
